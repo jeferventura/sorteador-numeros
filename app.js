96287be6d1d246a86adca4d644c3a58b29a41b1c
Jeferson Ventura <jefersonventura91@gmail.com>
2024-06-25 22:33:42 -0300

    start project

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function sortear(){
+    let quantidadeDeNumero = document.getElementById('quantidade').value;
+    let deNumero = document.getElementById('de').value;
+    let ateNumero = document.getElementById('ate').value;
+
+    alert(`quantidade: ${quantidadeDeNumero}`);
+    alert(`De numero: ${deNumero}`);
+    alert(`até Numero: ${ateNumero}`);
+};
+
